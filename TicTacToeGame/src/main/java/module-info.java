2abfa0e2702c93b53bc3cb08d2222dd182524300
Patrick Atef <patrickatefffa22@gmail.com>
2025-03-28 00:15:24 +0200

    Finished Score Board UI and integrated it with yasmeen's Game Board UI

---
+++ TicTacToeGame/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module com.mycompany.tictactoegame {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.base;
+    requires javafx.media; 
+    
+    opens com.mycompany.tictactoegame to javafx.fxml;
+    exports com.mycompany.tictactoegame;
+}
